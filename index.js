ae299f6f483092ecbf6b9c4cb13210feac446af5
Nikhil Seth <nikhilseth0078@gmail.com>
2024-05-14 10:59:29 -0400

    First Redux Store using Redux

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
+import { legacy_createStore as createStore } from "redux";
+
 console.log("Ready to start");
+
+const CAKE_ORDERED = "CAKE_ORDERED";
+
+//Action Creator
+function orderCake() {
+  return {
+    type: CAKE_ORDERED,
+    quantity: 1,
+  };
+}
+
+const initialState = {
+  numOfCakes: 10,
+  anotherProp: 0,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+console.log("initial state", store.getState());
+
+const unsubscribe = store.subscribe(() =>
+  console.log("updated state", store.getState())
+);
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+
+unsubscribe();
